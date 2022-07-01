85f1ced10a213948401858eb14ca431f78ab87b4
Yezid <ygm.mecatronico@gmail.com>
2022-07-01 16:27:03 -0500

    clase 8 : interactuando con JavaScript desde HTML

--- figuras.js
+++ figuras.js
@@ -79,3 +79,21 @@ function areaCirculo(radio){
 // console.log("El área del círculo es: " + areaCirculo + " cm^2");
 
 console.groupEnd();
+
+// Aquí interactuamos con el HTML
+
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert(area);
+}
