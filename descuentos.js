e3ff3cd74a115f7a768c6f36b70be94b8a2fe25c
Yezid <ygm.mecatronico@gmail.com>
2022-07-06 15:43:31 -0500

    clase 3: reto - funciones más inteligentes

--- descuentos.js
+++ descuentos.js
@@ -12,12 +12,30 @@ function calcularPrecioConDescuento(precio, descuento){
 }
 
 
+const coupons = [
+    {
+        name: "JuanDC_es_Batman",
+        discount: 15,
+    },
+    {
+        name: "pero_no_le_digas_a_nadie",
+        discount: 30,
+    },
+    {
+        name: "es_un_secreto",
+        discount: 25,
+    }
+];
+
 // console.log({
 //     precioOriginal,
 //     descuento,
 //     porcentajePrecioConDescuento,
 //     precioConDescuento
 // })
+
+
+
 
 function onClickButtonPriceDiscount() {
 
@@ -27,10 +45,24 @@ function onClickButtonPriceDiscount() {
     const inputDiscount = document.getElementById("InputDiscount");
     const discountValue = Number(inputDiscount.value);
 
-    const precioConDescuento = calcularPrecioConDescuento(priceValue,discountValue);
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+
+    const isCouponValueValid = function (coupon) {
+        return coupon.name === couponValue;
+    };    
+    const userCoupon = coupons.find(isCouponValueValid);
+    
+    let precioConDescuento;
+    if (!userCoupon) {
+        alert("El cupón '" + couponValue + "' no es válido");
+        precioConDescuento = calcularPrecioConDescuento(priceValue,discountValue);
+    } else {
+        const descuento = userCoupon.discount;
+        precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+    }    
 
     const resultP = document.getElementById("ResultP");
-
-    resultP.innerText = "El precio con descuento son $" + String(precioConDescuento);
+    resultP.innerText = "El precio con descuento son: $" + String(precioConDescuento);
 
 }
